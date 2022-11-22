4d454679db4f9f41fc37cd73edb5119b66a6a3a7
leyooow <98238999+leyooow@users.noreply.github.com>
2022-11-22 09:21:27 +0800

    asd

---
+++ spring_boot_sample/src/main/java/com/leo/spring_boot_sample/SpringBootSampleApplication.java
@@ -0,0 +1,24 @@
+package com.leo.spring_boot_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootSampleApplication {
+
+public static void main(String[] args) {
+SpringApplication.run(SpringBootSampleApplication.class, args);
+}
+
+@GetMapping("/hello")
+
+public String hello(@RequestParam(value = "name", defaultValue = "HEy") String name) {
+return String.format("Hello %s!", name);
+
+
+}
+}
