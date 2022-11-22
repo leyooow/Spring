fd1a7843fe8669bb8b1815bba095931b6c2c34d3
leyooow <98238999+leyooow@users.noreply.github.com>
2022-11-22 09:19:47 +0800

    asd

--- src/main/java/com/leo/spring_boot_sample/SpringBootSampleApplication.java
+++ src/main/java/com/leo/spring_boot_sample/SpringBootSampleApplication.java
@@ -15,7 +15,10 @@ SpringApplication.run(SpringBootSampleApplication.class, args);
 }
 
 @GetMapping("/hello")
-public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+public String hello(@RequestParam(value = "name", defaultValue = "HEy") String name) {
 return String.format("Hello %s!", name);
+
+
 }
 }
